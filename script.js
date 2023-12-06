396fe4695f5a1c13d1b0245f06ddd70a279998c3
Yonkou0 <sweash23@gmail.com>
2023-12-06 10:32:45 +0545

    Add maybe button functionality

--- script.js
+++ script.js
@@ -3,17 +3,23 @@ const question = document.querySelector(".question");
 const gif = document.querySelector(".gif");
 const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
+const mayBtn = document.querySelector(".maybe-btn")
 const currentTimeSpan = document.getElementById("current-time");
 const currentDateSpan = document.getElementById("current-date");
 
 function showAffection(message, gifFileName) {
     question.innerHTML = message;
-
-    // Assuming the GIFs are stored in the 'assets' folder
+    gif.style.width = '250px';
+    gif.style.height = '250px';
+    gif.style.objectFit = 'cover';
     const gifUrl = `./assets/${gifFileName}`;
     gif.src = gifUrl;
 }
-
+/*
+let totalYesCount = 0;
+let totalNoCount = 0;
+let totalMaybeCount = 0;
+*/
 function updateDateTime() {
     const now = new Date();
     const hours = now.getHours().toString().padStart(2, '0');
@@ -25,13 +31,67 @@ function updateDateTime() {
     currentDateSpan.textContent = formattedDate;
 }
 
+mayBtn.addEventListener("click", () => {
+    const maybeMessages = [
+        "NO, you should definitely click Yes or No!",
+        "Hmm, I'm waiting for your decision. Yes or No?",
+        "Maybe? You know you want to choose Yes or No!",
+    ];
+
+    const randomIndex = Math.floor(Math.random() * maybeMessages.length);
+    const maybeMessage = maybeMessages[randomIndex];
+
+    // Add some effect here if needed
+
+    showAffection(maybeMessage, "confused.gif");
+    /*
+ // Update total Maybe count
+ totalMaybeCount++;
+ document.getElementById("total-maybe").textContent = totalMaybeCount;
+*/
+
+    // Hide the maybeBtn after clicking
+    mayBtn.style.display = 'none';
+});
+
+const yesMessages = [
+    "Aaaaa, I like you too!",
+    "Yay! You're the best!",
+    "You made the right choice! I like you!",
+    "Oh, I was hoping you'd say that!",
+];
+
+const noMessages = [
+    "Ah, don't be like that! I like you.",
+    "You're breaking my heart! Say yes!",
+    "Please reconsider. I really like you.",
+    "Nooo, why would you say no? I like you so much!",
+];
+
 yesBtn.addEventListener("click", () => {
-    showAffection("Aaaaa, I like you too", "happy.gif");
+    const randomIndex = Math.floor(Math.random() * yesMessages.length);
+    const yesMessage = yesMessages[randomIndex];
+
+    showAffection(yesMessage, "happy.gif");
+     // Update total Yes count
+     /*
+     totalYesCount++;
+     document.getElementById("total-yes").textContent = totalYesCount;
+     */
 });
 
 noBtn.addEventListener("click", () => {
-    showAffection("Ah, I like you so much. Please love me.", "sad.gif");
+    const randomIndex = Math.floor(Math.random() * noMessages.length);
+    const noMessage = noMessages[randomIndex];
+
+    showAffection(noMessage, "sad.gif");
+    /*
+     // Update total No count
+     totalNoCount++;
+     document.getElementById("total-no").textContent = totalNoCount;
+     */
 });
+
 
 noBtn.addEventListener("mouseover", () => {
     const noBtnRect = noBtn.getBoundingClientRect();
